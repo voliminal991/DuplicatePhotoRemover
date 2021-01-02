26708a0701d51b51836b725c0506a83c05ce487b
voliminal991 <me@dgannon.co.uk>
2021-01-02 21:59:44 +0000

    Initial Commit

---
+++ DuplicatePhotoRemover/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DuplicatePhotoRemover
+//
+//  Created by David Gannon on 02/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DuplicatePhotoRemover/DuplicatePhotoRemoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuplicatePhotoRemoverApp.swift
+//  DuplicatePhotoRemover
+//
+//  Created by David Gannon on 02/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct DuplicatePhotoRemoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
